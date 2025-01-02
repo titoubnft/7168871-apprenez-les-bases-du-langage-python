f0fd5794c0ef0a01a535fe67975f1493f2112e59
titoubnft <titouaanbonenfant@gmail.com>
2025-01-02 15:32:34 +0100

    Update main.py

--- P2/P2C1/énoncé/main.py
+++ P2/P2C1/énoncé/main.py
@@ -1,1 +1,33 @@
-# Ecrivez votre code ici !
+nombre1 = input("20")
+nombre2 = input("22")
+
+if not nombre1.isnumeric() or not nombre2.isnumeric():
+    print("Erreur: les deux nombres doivent être des nombres entiers")
+    raise SystemExit("Fin du programme")
+
+nombre1 = int(nombre1)
+nombre2 = int(nombre2)
+
+operation = input("Entrez l'opération souhaitée ['+', '-', '*' ou '/']: ")
+
+if operation not in ["+", "-", "*", "/"]:
+    print("Erreur: le symbole d'opération doit être '+', '-', '*' ou '/'.")
+    raise SystemExit("Fin du programme")
+
+
+if operation == "+":    
+    resultat = nombre1 + nombre2
+elif operation == "-":
+    resultat = nombre1 - nombre2
+elif operation == "*":
+    resultat = nombre1 * nombre2
+elif operation == "/":
+
+    if nombre2 == 0:
+        print("Erreur: impossible de diviser par zéro.")
+        raise SystemExit("Fin du programme")
+
+    resultat = round(nombre1 / nombre2, 2)
+
+print(f"Le résultat de l'opération est: {round(resultat, 2)}")
+
